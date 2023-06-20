6bec0ea99d8ea2891cbe572667aa18ca0f1b372b
Arief Akhmad Fauzi <sekolah.arief@gmail.com>
2023-06-20 22:07:11 +0700

    clear debug

--- index.php
+++ index.php
@@ -14,11 +14,6 @@ $routeParts = explode('/', $route);
 $resource = array_shift($routeParts);
 $resource = strtok($resource, '?');
 
-var_dump($requestUri);
-var_dump($route);
-var_dump($routeParts);
-var_dump($resource);
-
 // Define the file path based on the resource
 $filePath = __DIR__ . '/routes/' . $resource . '.php';
 
