eb5c281a299f7e09cbb32eeadbe0e9f6079aea8b
Arief Akhmad Fauzi <sekolah.arief@gmail.com>
2023-06-20 21:30:44 +0700

    delete config

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Database configuration
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'your_database_name');
-define('DB_USER', 'your_username');
-define('DB_PASSWORD', 'your_password');
